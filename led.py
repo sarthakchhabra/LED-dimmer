59b7e0ce7b60b88fd95c404f570f1072417e60b1
sarthakchhabra <56037966+sarthakchhabra@users.noreply.github.com>
2019-10-02 19:54:35 +0530

    Create led.py

---
+++ led.py
@@ -0,0 +1,14 @@
+import RPi.GPIO as g
+import time
+g.setmode(g.BCM)
+g.setup(2,g.OUT)
+a=g.PWM(2,100)
+       
+a.start(0) 
+       
+for i in range (1,101):
+            a.ChangeDutyCycle(i)
+            time.sleep(0.01)
+for i in range (1,100):
+            a.ChangeDutyCycle(100-i)
+            time.sleep(0.01)
